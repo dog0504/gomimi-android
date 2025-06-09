c51b3e4e4c0af8ea68a4da7b41041a6a3819cfab
Hana <hana20948720@gmail.com>
2025-06-09 16:12:17 +0900

    遷移更新

--- app/src/main/java/com/example/test/LanguageSettingsActivity.kt
+++ app/src/main/java/com/example/test/LanguageSettingsActivity.kt
@@ -1,6 +1,8 @@
 package com.example.yourapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import com.example.test.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -12,5 +14,10 @@ class LanguageSettingsActivity: BaseActivity() {
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottomMenu)
         bottomNav.selectedItemId = R.id.navigation_settings
         setupBottomNav(bottomNav)
+
+        val backButton = findViewById<ImageView>(R.id.back_button)
+        backButton.setOnClickListener {
+            finish() // 現在のActivityを終了して前の画面に戻る
+        }
     }
 }
--- app/src/main/java/com/example/test/LocationSettingsActivity.kt
+++ app/src/main/java/com/example/test/LocationSettingsActivity.kt
@@ -1,6 +1,8 @@
 package com.example.yourapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import com.example.test.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -12,5 +14,12 @@ class LocationSettingsActivity: BaseActivity() {
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottomMenu)
         bottomNav.selectedItemId = R.id.navigation_settings
         setupBottomNav(bottomNav)
+
+        val backButton = findViewById<ImageView>(R.id.back_button)
+
+        backButton.setOnClickListener {
+            finish() // 現在のActivityを終了して前の画面に戻る
+        }
     }
+
 }
