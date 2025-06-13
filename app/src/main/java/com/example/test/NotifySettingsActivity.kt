72ee67423c8ebf647eac7e403451a8de7532d760
Hana <hana20948720@gmail.com>
2025-06-13 09:19:41 +0900

    通知設定ページ更新

--- app/src/main/java/com/example/test/NotifySettingsActivity.kt
+++ app/src/main/java/com/example/test/NotifySettingsActivity.kt
@@ -1,8 +1,10 @@
 package com.example.yourapp
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Spinner
 import com.example.test.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -19,6 +21,15 @@ class NotifySettingsActivity: BaseActivity() {
         backButton.setOnClickListener {
             finish() // 現在のActivityを終了して前の画面に戻る
         }
+        val spinner: Spinner = findViewById(R.id.spinner2)
+
+
+        val notifyOptions = listOf("前日", "1時間前", "30分前", "15分前")
+
+//  ArrayAdapter で Spinner
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, notifyOptions)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
 
     }
 }
