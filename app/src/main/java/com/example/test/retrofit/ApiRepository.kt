15f5f0a51a429a3d3e3bfb6f5dee8a4d94dc013f
Irbea192 <elepop777@gmail.com>
2025-06-26 20:46:14 +0900

    本番仕様のログイン処理を実装し、テスト用のシミュレーションコードをコメントアウト

--- app/src/main/java/com/example/test/retrofit/ApiRepository.kt
+++ app/src/main/java/com/example/test/retrofit/ApiRepository.kt
@@ -13,17 +13,25 @@ import java.io.IOException
 class UserRepository(private val userService: ApiService = RetrofitClient.instance.create(ApiService::class.java)) {
 
     // loginのシミュレーション
-    suspend fun login(email: String, password: String): NetworkResult<User> {
-        return withContext(Dispatchers.IO) {
-            delay(1000) // 1秒の遅延（ネットワーク遅延のシミュレーション）
+//    suspend fun login(email: String, password: String): NetworkResult<User> {
+//        return withContext(Dispatchers.IO) {
+//            delay(1000) // 1秒の遅延（ネットワーク遅延のシミュレーション）
+//
+//            if (email == "test@test.com" && password == "123") {
+//                NetworkResult.Success(User(id = "user123", name = "テストユーザー", email = "test@example.com", token = "mock_jwt_token_12345"))
+//            } else if (email == "error@example.com") {
+//                NetworkResult.Error("ログインに失敗しました: 不明なエラーが発生しました。")
+//            } else {
+//                NetworkResult.Error("ログインに失敗しました: メールアドレスまたはパスワードが違います")
+//            }
+//        }
+//    }
 
-            if (email == "test@test.com" && password == "123") {
-                NetworkResult.Success(User(id = "user123", name = "テストユーザー", email = "test@example.com", token = "mock_jwt_token_12345"))
-            } else if (email == "error@example.com") {
-                NetworkResult.Error("ログインに失敗しました: 不明なエラーが発生しました。")
-            } else {
-                NetworkResult.Error("ログインに失敗しました: メールアドレスまたはパスワードが違います")
-            }
+    // 本番仕様
+    suspend fun login(email: String, password: String): NetworkResult<AuthResponse> {
+        return safeApiCall {
+            val requestBody = LoginRequestBody(email, password)
+            userService.login(requestBody)
         }
     }
 
