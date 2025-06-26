fc2bc00756d82cf76c8eceea16cc807ff98d8c7d
Irbea192 <elepop777@gmail.com>
2025-06-26 20:46:53 +0900

    APIサービスまでのエンドポイント追加

--- app/src/main/java/com/example/test/retrofit/ApiService.kt
+++ app/src/main/java/com/example/test/retrofit/ApiService.kt
@@ -1,9 +1,9 @@
 package com.example.test.retrofit
 
-import com.example.test.retrofit.RecognitionResult
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Multipart
 import retrofit2.http.POST
@@ -23,4 +23,7 @@ interface ApiService {
         @Part image: MultipartBody.Part,
         @Part("description") description: RequestBody? = null
     ): Response<RecognitionResult>
+
+    @POST("auth/login")
+    suspend fun login(@Body requestBody: LoginRequestBody): Response<AuthResponse>
 }
