6d4bf8bf6cf078853f6563e2b349efe3f52999ca
Irbea192 <elepop777@gmail.com>
2025-06-29 00:44:32 +0900

    Add exact search API endpoint for manuals and update garbage identification endpoint

--- app/src/main/java/com/example/test/retrofit/ApiService.kt
+++ app/src/main/java/com/example/test/retrofit/ApiService.kt
@@ -9,6 +9,7 @@ import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ApiService {
     @GET("users/{id}")
@@ -28,10 +29,25 @@ interface ApiService {
     suspend fun login(@Body requestBody: LoginRequestBody): Response<AuthResponse>
 
     // ゴミ識別APIのエンドポイント
+//    @Multipart
+//    @POST("garbage/identify")
+//    suspend fun uploadImage(
+//        @Part image: MultipartBody.Part,
+//        @Part("description") description: RequestBody? = null
+//    ): Response<GarbageIdentificationResponse>
+    // テスト用ゴミ識別APIのエンドポイント
     @Multipart
-    @POST("garbage/identify")
+    @POST("garbage/identify/test")
     suspend fun uploadImage(
         @Part image: MultipartBody.Part,
         @Part("description") description: RequestBody? = null
     ): Response<GarbageIdentificationResponse>
+
+//    @GET("manuals/search")
+//    suspend fun searchManuals(@Query("keyword") keyword: String): Response<List<Manual>>
+
+    // 新しい完全一致検索APIの定義を追加
+    @GET("manuals/search/exact")
+    suspend fun searchManualExact(@Query("name") name: String): Response<Manual> // List<>を外し、単一のManualオブジェクトを受け取る
+
 }
